f55878376d5c461845bfa0db65036f95bc7bb4c5
noorulhuda789 <120157690+noorulhuda789@users.noreply.github.com>
2023-01-06 08:17:39 +0500

    Add files via upload

---
+++ task11.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main()
+{
+  int a;
+  cout<<"enter speed";
+  cin>>a;
+  if(a>100)
+  {  
+   cout<<"you have been challanged";
+  } 
+  if(a<=100)
+  {
+   cout<<"you are good";
+  }
+}
+
